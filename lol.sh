72a47f0e133ef78d757d126639a3515331fdf053
Harsh Vardhan Goswami <46974223+theuitown@users.noreply.github.com>
2020-01-10 00:54:16 +0530

    Add files via upload

---
+++ lol.sh
@@ -0,0 +1,21 @@
+pkg install figlet
+printf "$(tput setaf 1)"
+printf "================================="
+figlet "metasploit"
+printf "$(tput setaf 2)"
+figlet "android"
+printf "$(tput setaf 3)"
+figlet "iamharsh.dev" 
+printf "\n" 
+printf "=================================\n"
+printf "$(tput setaf 4)" 
+printf "\n" 
+printf "$(tput setaf 2)"
+apt install git
+printf "\n" 
+apt install wget
+printf "\n"
+apt install metasploit
+printf "\n" 
+printf "$(tput setaf 3)"
+msfconsole
